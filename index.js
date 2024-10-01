07d548cd923611e089fdb3c1a46fe3491d4e9412
Alan Romano <alan.rom25.95@gmail.com>
2024-10-01 16:39:37 -0400

    Add TraversalModes to index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.dfsFromNode = exports.dfs = exports.bfsFromNode = exports.bfs = void 0;
+exports.TraversalModes = exports.dfsFromNode = exports.dfs = exports.bfsFromNode = exports.bfs = void 0;
 const bfs_js_1 = require("./bfs.js");
 const dfs_js_1 = require("./dfs.js");
+const utils_js_1 = require("./utils.js");
 exports.bfs = bfs_js_1.bfs;
 exports.bfsFromNode = bfs_js_1.bfsFromNode;
 exports.dfs = dfs_js_1.dfs;
 exports.dfsFromNode = dfs_js_1.dfsFromNode;
+exports.TraversalModes = utils_js_1.TraversalModes;
